8376f00f22d103f0fc037ba2564db11e38f7564e
euro20179 <sandollar2020@gmail.com>
2025-08-19 20:58:33 -0700

    spaces

--- server.py
+++ server.py
@@ -35,16 +35,35 @@ class LinkedList[T]:
 
 type spacetype_t = int
 
+ST_PROPERTY: spacetype_t = 0
+ST_COMMUNITY_CHEST: spacetype_t = 1
+ST_CHANCE: spacetype_t = 2
+ST_JAIL: spacetype_t = 3
+ST_FREE_PARKING: spacetype_t = 4
+ST_GOTO_JAIL: spacetype_t = 5
+ST_GO: spacetype_t = 6
+ST_LUXURY_TAX: spacetype_t = 7
+ST_INCOME_TAX: spacetype_t = 8
+ST_RAILROAD: spacetype_t = 9
+
 class Player:
     ...
 
-class Space(ABC):
-    def onland(self, player: Player) -> None:
-        pass
+class Space:
+    spaceType: spacetype_t
+    def __init__(self, spaceType: spacetype_t, *, cost: int, name: str):
+        self.spaceType = spaceType
 
-class Space_Property(Space):
-    cost: int
-    
+    def onland(self, player: Player):
+        {
+            ST_RAILROAD: onland_railroad,
+        }[self.spaceType](self, player)
+
+def onland_railroad(rr: Space, player: Player):
+    pass
+
+
+Space(ST_RAILROAD, cost=100, name="pennsylvania ave")
 
 class Board:
     state: LinkedList[Space]
