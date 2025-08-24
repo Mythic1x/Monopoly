e78e81298f8de7e03d0e7ac50bcad1f3150bbde0
euro20179 <sandollar2020@gmail.com>
2025-08-23 17:45:22 -0700

    spaces

--- client/src/components/board.tsx
+++ client/src/components/board.tsx
@@ -4,14 +4,24 @@ interface Props {
     board: Board
 }
 
-function GameBoard({ board }: Props) {
+function Space({ space }: { space: Space }) {
     return <>
-        <div className="board">{board.spaces.map((space: Space) =>
-            <div className="space">
-                <span className="name">{space.name}</span>
-                <span className="cost">{space.cost}</span>
-            </div>
-        )}</div>
+        <div className="space">
+            <span className="name">{space.name}</span>
+            <span className="cost">{space.cost}</span>
+        </div>
+    </>
+}
+
+function GameBoard({ board }: Props) {
+    console.log(board)
+    return <>
+        <div className="board">
+            <div className="bottom">{board.spaces.slice(0, 10).map(space => <Space space={space} />)}</div>
+            <div className="left">{board.spaces.slice(10, 20).map(space => <Space space={space} />)}</div>
+            <div className="top">{board.spaces.slice(20, 30).map(space => <Space space={space} />)}</div>
+            <div className="right">{board.spaces.slice(30).map(space => <Space space={space} />)}</div>
+        </div>
     </>
 }
 
