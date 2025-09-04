7bf5130e8e20dd827523a169ff4082f591a5ea72
euro20179 <sandollar2020@gmail.com>
2025-09-04 15:59:01 -0700

    fix: custom ip broken

--- client/src/components/PlayerSetup.tsx
+++ client/src/components/PlayerSetup.tsx
@@ -7,15 +7,13 @@ function PlayerSetup({ onSetupComplete }: any) {
     const [piece, setPiece] = useState('');
     const { ip, setIp } = useContext(ConnectionContext)
 
-    setIp("0.0.0.0")
     const ipIn = useRef<HTMLInputElement>(null)
 
     const handleSubmit = (e: FormEvent) => {
-        e.preventDefault();
-        setIp(ipIn.current.value)
-        if (!name || !piece) return;
-        setIp(`http://${ip}:8765`)
-        localStorage.setItem('cachedIp', ip);
+        let val = ipIn.current.value || "0.0.0.0"
+
+        setIp(`http://${val}:8765`)
+        localStorage.setItem('cachedIp', val);
         onSetupComplete({ name, piece });
     };
 
@@ -23,13 +21,14 @@ function PlayerSetup({ onSetupComplete }: any) {
         <center><h1>Fucking Login screen</h1><p>
             Not fucking ai generated, get out with your ai generated login screens.
         </p></center>
-        <form onSubmit={handleSubmit} className="selection-box">
+        <form onSubmit={handleSubmit} className="selection-box" action={void(0)}>
             <input
                 type="text"
                 className="name-input"
                 placeholder='Enter your name'
                 value={name}
                 onChange={(e) => setName(e.target.value)}
+                required
             />
             <input
                 type="text"
@@ -37,6 +36,7 @@ function PlayerSetup({ onSetupComplete }: any) {
                 placeholder='Enter your piece'
                 value={piece}
                 onChange={(e) => setPiece(e.target.value)}
+                required
             />
             <input type="text" className="ip-selection" placeholder="0.0.0.0" ref={ipIn} />
             <button type="submit">Join Game</button>
