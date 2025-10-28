8205631d20fb6040417644d1802c80d751d92216
euro20179 <sandollar2020@gmail.com>
2025-10-28 00:26:32 -0700

    code: track files

---
+++ monopolytypes.py
@@ -0,0 +1,5 @@
+
+type player_t = str
+type space_t = int
+type loan_t = float
+
---
+++ status.py
@@ -0,0 +1,119 @@
+from dataclasses import dataclass
+from monopolytypes import *
+
+class status_t:
+    broadcast: bool = False
+
+@dataclass
+class DUE_LOAN(status_t):
+    loan: loan_t
+    player: player_t
+
+@dataclass
+class BANKRUPT(status_t):
+    player: player_t
+
+@dataclass
+class DRAW_CHANCE(status_t):
+    event: str
+    player: player_t
+    
+@dataclass
+class FAIL(status_t):
+    player: player_t
+    
+@dataclass 
+class MORTGAGE_SUCCESS(status_t):
+    player: player_t
+    space: space_t
+    
+@dataclass 
+class UNMORTGAGE_SUCCESS(status_t):
+    player: player_t
+    space: space_t
+
+@dataclass
+class BUY_FAIL(status_t):
+    space: space_t
+
+@dataclass
+class BUY_HOUSE_FAIL(status_t):
+    space: space_t
+
+@dataclass
+class BUY_HOTEL_FAIL(status_t):
+    space: space_t
+
+@dataclass
+class BUY_SUCCESS(status_t):
+    space: space_t
+
+@dataclass
+class BUY_HOUSE_SUCCESS(status_t):
+    space: space_t
+    broadcast: bool = True
+
+@dataclass
+class BUY_HOTEL_SUCCESS(status_t):
+    space: space_t
+    broadcast: bool = True
+
+@dataclass
+class BUY_NEW_SET(status_t):
+    space: space_t
+    broadcast: bool = True
+
+@dataclass
+class PROMPT_TO_BUY(status_t):
+    space: space_t
+
+@dataclass
+class MONEY_LOST(status_t):
+    #FIXME this needs a player so that the ui can say which player lost money lmfao
+    amount: int
+    broadcast: bool = True
+
+@dataclass
+class MONEY_GIVEN(status_t):
+    amount: int
+    broadcast: bool = True
+    
+@dataclass 
+class AUCTION_END(status_t):
+    space: space_t
+    auction_dict: dict
+    broadcast: bool = True
+
+@dataclass
+class NONE(status_t):
+    pass
+
+@dataclass
+class PAY_OTHER(status_t):
+    amount: int
+    payer: player_t
+    other: player_t
+    broadcast: bool = True
+
+@dataclass
+class PAY_TAX(status_t):
+    amount: int
+    taxname: str
+    broadcast: bool = True
+
+@dataclass
+class PASS_GO(status_t):
+    earned: int
+    broadcast: bool = True
+
+@dataclass
+class PAY_JAIL(status_t):
+    cost: int
+    broadcast: bool = True
+
+
+@dataclass
+class Chance:
+    event: str
+    type: str
+    data: Any
